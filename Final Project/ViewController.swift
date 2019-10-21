1e30250d8c89737684151113e30ab654140530d6
Asepabdaz <asepabdaz@gmail.com>
2019-10-21 11:46:51 +0700

    Initial Commit

---
+++ Final Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Project
+//
+//  Created by Hai on 21/10/19.
+//  Copyright © 2019 Organic Waste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
