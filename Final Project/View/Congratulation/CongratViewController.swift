9f4d24a1d44958bc99fdd9d0deb270580aaedf3e
Asepabdaz <asepabdaz@gmail.com>
2019-11-25 09:17:24 +0700

    add congratulation files

---
+++ Final Project/View/Congratulation/CongratViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CongratViewController.swift
+//  Final Project
+//
+//  Created by Hai on 25/11/19.
+//  Copyright © 2019 Organic Waste. All rights reserved.
+//
+
+import UIKit
+
+class CongratViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
